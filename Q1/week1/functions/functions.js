e28229b392b09515cf309f605827730b91682edf
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-08-24 10:55:06 -0400

    modified func

--- Q1/week1/functions/functions.js
+++ Q1/week1/functions/functions.js
@@ -1,3 +1,7 @@
-function myFunc() {
-  console.log('Your machine is infected!!');
+
+function personalVirus(myName, yourName) {
+  console.log('Your machine is infected ' + myName + '!!!');
+  console.log('Your machine is infected ' + yourName + '!!!');
 }
+//have to invoke the function for it to run in the iterm
+personalVirus('Surbhi', 'Jen');
