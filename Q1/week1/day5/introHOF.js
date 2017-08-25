da5e7058f59dfc00be70c8e34f646785d46a5e27
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-08-25 10:54:06 -0400

    Higher order func

---
+++ Q1/week1/day5/introHOF.js
@@ -0,0 +1,15 @@
+//Function Declaration
+// function myFunc() {
+//   console.log('Good morning g65!');
+// }
+//
+// myFunc();
+
+//Function Expression
+// console.log(myFunc2); // will return undefined
+// var myFunc2 = function () {
+//   console.log('TGIF');
+// };
+//
+// myFunc2();//if this variable invocation appears before the expression, it will return an error.
+setInterval(function () {alert('HI!');}, 3000);
